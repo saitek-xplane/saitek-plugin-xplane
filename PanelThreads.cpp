2f3c1d2110622a9591c8ed3fed5b6db15dea99d4
jpoirier <>
2010-11-16 15:05:50 -0600

    push and tag before switching to libusb

--- PanelThreads.cpp
+++ PanelThreads.cpp
@@ -110,42 +110,188 @@ void to_bytes(unsigned char* c, unsigned long long v) {
     }
 }
 
+/*
+    tuning:
+        clockwise           28 00 00
+        counter clockwise   48 00 00
+    pitch
+        up                  08 80 04
+        neutral             08 80 00
+        down                08 80 08
+    auto throttle
+        arm                 08 80 00
+        off                 08 00 00
+    flaps
+        up                  08 80 01
+        neutral             08 80 00
+        down                08 80 02
+    IAS knob
+        hdg                 08 00 00
+        ias                 04 00 00
+        vs                  02 00 00
+        alt                 01 00 00
+        crs                 10 00 00
+    button
+        ap                  10 01 00
+        hdg                 10 02 00/10 80 00
+        nav                 10 04 00
+        ias                 10 08 00
+        alt                 10 10 00
+        vs                  10 20 00
+        apr                 10 40 00
+        rev                 10 80 00
+*/
+
+#define        CLOCKWISE           0x05800050
+#define        COUNTERCLOCKWISE   0x03080050
+ //   pitch
+#define        PITCHUP                  0x01080450
+#define        PITCHDOWN                0x01080850
+//    auto throttle
+#define        ARMSWTH                 0x000100050
+//#define        OFFSWTH                 0x
+//    flaps
+#define        FLAPSUP                  0x10800150
+#define        FLAPSDOWN                0x01080250
+//     knob
+#define        HDGKNB                 0x00880050
+#define        IASKNB                 0x00480050
+#define        VSKNB                  0x00280050
+#define        ALTKNB                 0x00180050
+#define        CRSKNB                 0x01080050
+//    button
+#define        AP                  0x08180050
+#define        HDG                 0x00181050
+#define        NAV                 0x00182050
+#define        IAS                 0x00184050
+#define        ALT                 0x00188050
+#define        VS                  0x00190050
+#define        APR                 0x001A0050
+#define        REV                 0x00100050
+
 /**
  *
  */
 void FromPanelThread::execute() {
 //    memset(outBuf, 0, OUT_BUF_CNT);
     unsigned char* x;
+    unsigned int y, a, b, c, d;
+
+ char data[20];
 
     while (threads_run) {
         state->wait();
-//psleep(300);
-//if (product == RP_PROD_ID) {
-////    gRpTrigger.wait();
-//    XPLMSpeakString("radio\n");
-//} else if (product == MP_PROD_ID) {
-////    gMpTrigger.wait();
-//    XPLMSpeakString("multi\n");
-//} else if (product == SP_PROD_ID) {
-////    gSpTrigger.wait();
-//    XPLMSpeakString("switch\n");
-//}
 
-    if (hid) {
-        if ((res = hid_read((hid_device*)hid, buf, HID_READ_CNT)) <= 0) {
-            if (res == HID_DISCONNECTED)
+        if (hid) {
+            if ((res = hid_read((hid_device*)hid, buf, HID_READ_CNT)) <= 0) {
+                if (res == HID_DISCONNECTED)
 //XPLMSpeakString("disconnected");
-                psleep(100);
+                    psleep(100);
+                continue;
+            }
+        } else {
             continue;
         }
-    } else {
-        continue;
-    }
 
-    x = (unsigned char*) malloc(sizeof(unsigned char));
-    *x = 1;
+        sprintf(data, "%x%x%x%x", buf[0], buf[1], buf[2], buf[3]);
+        a = buf[0];
+        b = buf[1];
+        c = buf[2];
+        d = buf[3];
+        y =  a << 24 || b << 16 || c << 8 || d;
+
+        switch (y) {
+            case CLOCKWISE:
+                XPLMSpeakString("clockwise \n");
+                break;
+            case COUNTERCLOCKWISE:
+                XPLMSpeakString("counter clockwise \n");
+                break;
+            case PITCHUP:
+                XPLMSpeakString("pitch up \n");
+                break;
+    //        case PITCHNEUTRAL:
+    //            XPLMSpeakString("pitch neutral \n");
+    //            break;
+            case PITCHDOWN:
+                XPLMSpeakString("pitch down \n");
+                break;
+    //        case ARMSWTH:
+    //            XPLMSpeakString("autothrottle arm \n");
+    //            break;
+    //        case OFFSWTH:
+    //            XPLMSpeakString("autothrottle off \n");
+    //            break;
+            case FLAPSUP:
+                XPLMSpeakString("flaps up \n");
+                break;
+    //        case FLAPSNEUTRAL:
+    //            XPLMSpeakString("flaps neutral \n");
+    //            break;
+            case FLAPSDOWN:
+                XPLMSpeakString("flaps down \n");
+                break;
+            case HDGKNB:
+                XPLMSpeakString("heading switch \n");
+                break;
+            case IASKNB:
+                XPLMSpeakString("inicated airspeed switch \n");
+                break;
+            case VSKNB:
+                XPLMSpeakString("vertical speed  switch \n");
+                break;
+            case ALTKNB:
+                XPLMSpeakString("altitude switch \n");
+                break;
+            case CRSKNB:
+                XPLMSpeakString("course switch \n");
+                break;
+    //push buttons
+            case AP:
+                XPLMSpeakString("approach \n");
+                break;
+            case HDG:
+                XPLMSpeakString("heading \n");
+                break;
+            case NAV:
+                XPLMSpeakString("navigation \n");
+                break;
+            case IAS:
+                XPLMSpeakString("indicated ait speed \n");
+                break;
+            case ALT:
+                XPLMSpeakString("altitude \n");
+                break;
+            case VS:
+                XPLMSpeakString("vertical speed \n");
+                break;
+            case APR:
+                XPLMSpeakString("approach \n");
+                break;
+            case REV:
+                XPLMSpeakString("reverse \n");
+                break;
+            default:
+                XPLMSpeakString(data);
+                break;
+        }
+
+
+//    if (product == RP_PROD_ID) {
+//    //    gRpTrigger.wait();
+//        XPLMSpeakString("radio\n");
+//    } else if (product == MP_PROD_ID) {
+//    //    gMpTrigger.wait();
+//        XPLMSpeakString("multi\n");
+//    } else if (product == SP_PROD_ID) {
+//    //    gSpTrigger.wait();
+//        XPLMSpeakString("switch\n");
+//    }
+
+        x = (unsigned char*) malloc(sizeof(unsigned char));
+        *x = 1;
 //XPLMSpeakString("posting\n");
-    ijq->post(new myjob(x));
+        ijq->post(new myjob(x));
 
 //        message* msg = ojq->getmessage(MSG_NOWAIT);
 
@@ -176,9 +322,9 @@ void ToPanelThread::execute() {
         state->wait();
 
 // todo: res processing
-//        unsigned char* x = (unsigned char*) malloc(sizeof(unsigned char));
-//        *x = 1;
-//        ijq->post(new myjob(x));
+        unsigned char* x = (unsigned char*) malloc(sizeof(unsigned char));
+        *x = 1;
+        ijq->post(new myjob(x));
 
         msg = ijq->getmessage(MSG_WAIT);
 //XPLMSpeakString("received\n");
